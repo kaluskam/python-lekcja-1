303a4289ab51de3c6691cc13108cfeea65cd5a4a
jany0 <jany0711@op.pl>
2025-12-18 12:38:23 +0100

    1 lekcja Homework

--- main.py
+++ main.py
@@ -2,17 +2,27 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 name=input("Enter your name: ")
 age=int(input("Enter your age: "))
 age2=input("Enter your age: ")
+name2=input("Enter your name2: ")
+yearofbirth=int(input("Enter your year of birth: "))
+city=input("Enter your city: ")
+profession=input("Enter your profession: ")
+oneskillyouwanttolearn=input("Enter your one skill you want to learn: ")
+
     # Use a breakpoint in the code line below to debug your script.
 print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 print("Hi",',', name,sep='',end='!\n')
 print(f'your age is {age}')
 print(f'type of age {type(age)}, {age+10}' )
 print(f'type of age2 {type(age2)}, {age2+"10"}' )
-
+print(f"Hi,{name2}")
+print(f"your year of birth is {yearofbirth}")
+print(f"your city is {city}")
+print(f"your profession is {profession}")
+print(f"your one skill you want to learn is {oneskillyouwanttolearn}")
+print('jest dzien')
 # Press the green button in the gutter to run the script.
 
 
